01cacbe9588a884b132905207dc4c8593e2f18bc
Ken Lee Shu Ming <kenlsm@outlook.com>
2018-10-24 18:22:35 +0800

    add keypress, beautify

---
+++ keypressUtils.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+const handleKeyPress = ref => {
+	process.stdin.on('keypress', (str, key) => {
+		ref[0] = str;
+		if (str === 'q') process.exit();
+	});	
+}
+
+module.exports = {
+	handleKeyPress,
+};
