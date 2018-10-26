a59c4885383c1a11ef22c8b886147107d60356f6
Ken Lee Shu Ming <kenlsm@outlook.com>
2018-10-26 20:08:20 +0800

    add rc

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": "airbnb-base",
+  rules: {
+    "no-bitwise": 0,
+    "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+    "arrow-parens": ["error", "as-needed"],
+  },
+};
